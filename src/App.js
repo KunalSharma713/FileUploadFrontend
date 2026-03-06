31b7c2c374951d5fed792388e2b1d539c7a32110
Kunal Sharma <kunalsharma@startbitsolutions.com>
2026-03-06 18:27:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import FileUpload from "./FileUpload";
 
 function App() {
-  
   // Replace this with actual logged-in user ID from your auth system
   const userId = "user123";
 
